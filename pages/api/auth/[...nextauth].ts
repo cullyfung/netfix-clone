959c8aff917156ea9b7fcc99c0e81e8e0d592c30
cullyfung <cullyfung@gamil.com>
2023-03-24 21:15:41 +0800

    feat: auth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,63 @@
+import NextAuth from 'next-auth'
+import Credentials from 'next-auth/providers/credentials'
+import { compare } from 'bcrypt'
+import prismadb from '@/lib/prismadb'
+
+export default NextAuth({
+  providers: [
+    Credentials({
+      id: 'credentials',
+      name: 'Credentials',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'text',
+          placeholder: 'Email',
+          required: true
+        },
+        password: {
+          label: 'Password',
+          type: 'password',
+          placeholder: 'Password',
+          required: true
+        }
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error('Email or password required')
+        }
+        const user = await prismadb.user.findUnique({
+          where: {
+            email: credentials.email
+          }
+        })
+
+        if (!user || !user.hashedPassword) {
+          throw new Error('Email not exist')
+        }
+
+        const isCorrectPassword = await compare(
+          credentials.password,
+          user.hashedPassword
+        )
+
+        if (!isCorrectPassword) {
+          throw new Error('Incorrect password')
+        }
+
+        return user
+      }
+    })
+  ],
+  pages: {
+    signIn: '/auth'
+  },
+  debug: process.env.NODE_ENV === 'development',
+  session: {
+    strategy: 'jwt'
+  },
+  jwt: {
+    secret: process.env.NEXTAUTH_JWT_SECRET
+  },
+  secret: process.env.NEXTAUTH_SECRET
+})
