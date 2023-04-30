3bf6f690be89d6d98857308a79f2e1dccf1f030a
cullyfung <cullyfung@gamil.com>
2023-04-30 19:47:52 +0800

    feat: protecting routes,profiles screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,28 @@
+import { NextPageContext } from 'next'
+import { getSession, signOut } from 'next-auth/react'
+
+import useCurrentUser from '@/hooks/useCurrentUser'
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context)
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
+
 export default function Home() {
+  const { data: user } = useCurrentUser()
+
   return (
     <>
       <h1>Netflix Clone</h1>
