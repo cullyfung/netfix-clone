70b4eb53495db69bcd904d710d85ef14f66d78eb
cullyfung <cullyfung@gamil.com>
2023-05-22 23:37:15 +0800

    feat: info modal

---
+++ pages/watch/[movieId].tsx
@@ -0,0 +1,43 @@
+import useMovie from '@/hooks/useMovie'
+import { useRouter } from 'next/router'
+import React from 'react'
+
+import { AiOutlineArrowLeft } from 'react-icons/ai'
+
+const Watch = () => {
+  const router = useRouter()
+  const { movieId } = router.query
+
+  const { data } = useMovie(movieId as string)
+  return (
+    <div className="w-screen h-screen bg-black">
+      <nav
+        className="
+        fixed
+        w-full
+        p-4
+        z-10
+        flex
+        flex-row
+        items-center
+        gap-8
+        bg-black
+        bg-opacity-70
+      "
+      >
+        <AiOutlineArrowLeft
+          onClick={() => router.push(`/`)}
+          className="text-white cursor-pointer"
+          size={40}
+        />
+
+        <p className="text-white text-xl md:text-3xl font-bold">
+          <span className="font-light">Watching:</span>
+          {data?.title}
+        </p>
+      </nav>
+    </div>
+  )
+}
+
+export default Watch
